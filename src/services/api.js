6d5839016bb2d36f1fd6065bdcd027e7f90bee24
Appunram <appunram@gmail.com>
2021-06-12 10:58:41 +0530

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'https://event-managementapi.herokuapp.com/'
+    baseURL:'https://event-managementapi.herokuapp.com'
 })
 
 api.interceptors.request.use(
